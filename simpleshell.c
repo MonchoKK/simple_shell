fec9bf775ddeacefc5e0b7840cce68fbb74a1836
maestrozx <liberiusongere@gmail.com>
2024-02-29 00:08:26 +0300

    Simple shell

--- simpleshell.c
+++ simpleshell.c
@@ -1,64 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
 
-#define X_OK 1
-#define F_OK 0
 #define MAX_ARGS 20
 
-extern char **env;
+/**
+ * tokenizer - converts a string into tokens.
+ * @lineptr: the string to be tokenized.
+ * Return: returns an array of converted tokens.
+ */
+char **tokenizer(char *lineptr)
+{
+	char **args = malloc(MAX_ARGS * sizeof(char *));
 
-int main(void) {
-    char *lineptr = NULL;
-    size_t len = 0;
-    ssize_t read;
-    pid_t pid;
-    int status;
+	int i = 0;
 
-    while (1) {
-        printf("$ ");
-        read = getline(&lineptr, &len, stdin);
+	args[i] = strtok(lineptr, " ");
 
-        if (read == -1 || strcmp(lineptr, "exit\n") == 0) {
-            printf("\n");
-            break;
-        }
+	while (args[i] != NULL && i < MAX_ARGS - 1)
+	{
+		args[++i] = strtok(NULL, " ");
+	}
+	args[i] = NULL;
 
-        int str_count = strlen(lineptr);
-        lineptr[str_count - 1] = '\0';
-
-        char *argv[MAX_ARGS];
-        int i = 0;
-
-        argv[i] = strtok(lineptr, " ");
-
-        while (argv[i] != NULL && i < MAX_ARGS - 1) {
-            argv[++i] = strtok(NULL, " ");
-        }
-        argv[i] = NULL;
-
-        if (access(argv[0], X_OK) == 0 && !access(argv[0], F_OK)) {
-            pid = fork();
-            if (pid == -1) {
-                perror("Fork Failed");
-                exit(EXIT_FAILURE);
-            }
-            if (pid == 0) {
-                execve(argv[0], argv, env);
-            } else {
-                waitpid(pid, &status, 0);
-
-                if (WIFEXITED(status) && WEXITSTATUS(status) == 127) {
-                    printf("Command not found: %s\n", argv[0]);
-                }
-            }
-        } else {
-            printf("Command not found or not executable: %s\n", argv[0]);
-        }
-    }
-
-    free(lineptr);
-    return 0;
+	return (args);
 }
+
+/**
+ * execute_command - executes the passed command.
+ * @args: the commands to be executed.
+ * Return: Nothing.
+ */
+void execute_command(char **args)
+{
+	pid_t pid;
+	int status;
+
+	if (access(args[0], X_OK) == 0 && !access(args[0], F_OK))
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Fork Failed");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+			execve(args[0], args, NULL);
+		else
+		{
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
+				printf("Command not found: %s\n", args[0]);
+		}
+	}
+	else
+	{
+		printf("Command not found or not executable: %s\n", args[0]);
+	}
+}
+
+/**
+ * main - main shell loop.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *lineptr = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char **args;
+	int str_count;
+
+	while (1)
+	{
+		printf("$ ");
+		read = getline(&lineptr, &len, stdin);
+
+		if (read == -1 || strcmp(lineptr, "exit\n") == 0)
+		{
+			printf("\n");
+			break;
+		}
+
+		str_count = strlen(lineptr);
+
+		lineptr[str_count - 1] = '\0';
+
+		args = tokenizer(lineptr);
+
+		execute_command(args);
+	}
+	free(lineptr);
+	return (0);
+}
+
