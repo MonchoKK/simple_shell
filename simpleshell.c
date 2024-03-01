35330d910d601ae2b130c75d45acf9d3199e703d
maestrozx <liberiusongere@gmail.com>
2024-03-01 17:03:09 +0300

    Simple Shell

--- simpleshell.c
+++ simpleshell.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 
-#define MAX_ARGS 20
+#define DELIM " \t\n"
+#define PROMPT "$ "
+#define MAX_ARGS 30
+
+/**
+ * print_environment - function printing the environment
+ *
+ * Return: Always 0.
+ */
+void print_environment(void)
+{
+	extern char **environ;
+	char **env = environ;
+
+	while (*env)
+	{
+		printf("%s\n", *env++);
+	}
+}
 
 /**
  * tokenizer - converts a string into tokens.
- * @lineptr: the string to be tokenized.
- * @delim: delimiter to tokenize the string.
+ * @line: the string to be tokenized.
+ * @delim: delimiter to separate strings.
  * Return: returns an array of converted tokens.
  */
-char **tokenizer(char *lineptr, char *delim)
+char **tokenizer(char *line, char *delim)
 {
 	int i = 0;
 	char **args = malloc((MAX_ARGS + 1) * sizeof(char *));
@@ -22,10 +40,9 @@ char **tokenizer(char *lineptr, char *delim)
 		perror("malloc failed");
 		exit(EXIT_FAILURE);
 	}
+	args[i] = strtok(line, delim);
 
-	args[i] = strtok(lineptr, delim);
-
-	while (args[i] != NULL && i < MAX_ARGS - 1)
+	while (args[i] != NULL)
 	{
 		args[++i] = strtok(NULL, delim);
 	}
@@ -35,72 +52,85 @@ char **tokenizer(char *lineptr, char *delim)
 }
 
 /**
- * execute_command - executes the passed command.
- * @args: the commands to be executed.
- * Return: Nothing.
+ * execute - uses execve to execute command
+ * @args: array of arguments to be executed
+ *
+ * Return: 1 on success, 0 on exit and -1 on error
  */
-void execute_command(char **args)
+int execute(char **args)
 {
 	pid_t pid;
 	int status;
 
+	if (strcmp(args[0], "exit") == 0)
+	{
+		return (0);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		print_environment();
+		return (1);
+	}
 	pid = fork();
 	if (pid == -1)
 	{
-		perror("Fork Failed");
-		exit(EXIT_FAILURE);
+		perror("fork failed");
+		return (-1);
 	}
 	if (pid == 0)
 	{
 		if (execve(args[0], args, NULL) == -1)
 		{
-			perror("Execve failed");
-			exit(EXIT_FAILURE);
+			perror(args[0]);
+			exit(1);
 		}
 	}
 	else
 	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
-			printf("Command not found: %s\n", args[0]);
+		wait(&status);
 	}
+	return (1);
 }
 
 /**
- * main - main shell loop.
+ * main - a simple shell loop
  *
- * Return: Always 0.
+ * Return: 0 on success, 1 on error
  */
 int main(void)
 {
-	char *lineptr = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char **args;
-	int str_count;
+	char *line, **args;
+	int i;
+	ssize_t len = 0;
+
+	line = NULL;
 
 	while (1)
 	{
-		printf("$ ");
-		read = getline(&lineptr, &len, stdin);
-
-		if (read == -1 || strcmp(lineptr, "exit\n") == 0)
+		printf(PROMPT);
+		if (getline(&line, &len, stdin) == -1)
 		{
-			printf("\n");
-			break;
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("getline");
+				return (1);
+			}
 		}
+		args = tokenizer(line, DELIM);
+		int status = execute(args);
 
-		str_count = strlen(lineptr);
-
-		if (str_count > 0 && lineptr[str_count - 1] == '\n')
-			lineptr[str_count - 1] = '\0';
-
-		args = tokenizer(lineptr, " ");
-
-		execute_command(args);
-
-		free(args);
+		if (status == 0)
+			break;
+		if (status == -1)
+			return (1);
 	}
-	free(lineptr);
+	free(line);
+	free(args);
 	return (0);
 }
+
